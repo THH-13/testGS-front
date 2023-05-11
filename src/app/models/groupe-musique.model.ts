e12dc5f23646794874f95779463071845e6ed47d
Tara-2019 <hachem.huet.tara@gmail.com>
2023-05-11 10:10:34 +0200

    groupe-musique model

---
+++ src/app/models/groupe-musique.model.ts
@@ -0,0 +1,12 @@
+export class GroupeMusiqueModel {
+  id?: number;
+  nomGroupe?: string;
+  origine?: string;
+  ville?: string;
+  anneeDebut?: Date;
+  anneeSeparation?: Date;
+  fondateurs?: string;
+  membres?: number;
+  courantMusical?: string;
+  presentation?: string;
+}
