1a06f9187a7bd9c4a227a8225cc6f30079a0e34f
Tara-2019 <hachem.huet.tara@gmail.com>
2023-05-11 10:15:33 +0200

    page non trouvee

---
+++ src/app/non-trouve/non-trouve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-trouve',
+  templateUrl: './non-trouve.component.html',
+  styleUrls: ['./non-trouve.component.css']
+})
+export class NonTrouveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
