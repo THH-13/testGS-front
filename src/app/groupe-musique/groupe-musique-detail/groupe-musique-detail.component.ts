b0415dad2620d2676f138e22db353473cf0af788
Tara-2019 <hachem.huet.tara@gmail.com>
2023-05-12 11:22:30 +0200

    formulaire update groupe musique

--- src/app/groupe-musique/groupe-musique-detail/groupe-musique-detail.component.ts
+++ src/app/groupe-musique/groupe-musique-detail/groupe-musique-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GroupeMusiqueService} from "../liste-groupe-musique/groupe-musique.service";
 import {ActivatedRoute} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {GroupeMusiqueModel} from "../../models/groupe-musique.model";
 
 @Component({
   selector: 'app-groupe-musique-detail',
@@ -8,15 +10,38 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./groupe-musique-detail.component.css']
 })
 export class GroupeMusiqueDetailComponent implements OnInit {
-  groupeMusique: any;
   id = this.actRoute.snapshot.params['id'];
+  formGroup!: FormGroup;
 
   constructor(private groupeMusiqueService: GroupeMusiqueService, public actRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    let groupeMusique = new GroupeMusiqueModel();
     this.groupeMusiqueService.fetchByIdGroupeMusique(this.id).subscribe(data => {
-      this.groupeMusique = data;
+      groupeMusique = data;
+      this.formGroup = new FormGroup({
+        "nomGroupe": new FormControl(groupeMusique.nomGroupe),
+        "origine": new FormControl(groupeMusique.origine),
+        "ville": new FormControl(groupeMusique.ville),
+        "anneeDebut": new FormControl(groupeMusique.anneeDebut),
+        "anneeSeparation": new FormControl(groupeMusique.anneeSeparation),
+        "fondateurs": new FormControl(groupeMusique.fondateurs),
+        "membres": new FormControl(groupeMusique.membres),
+        "courantMusical": new FormControl(groupeMusique.courantMusical),
+        "presentation": new FormControl(groupeMusique.presentation),
+      });
     });
+  }
+
+  onSubmit() {
+    if (this.formGroup && this.formGroup.valid) {
+      this.groupeMusiqueService.updateGroupeMusique(this.id, this.formGroup.value).subscribe();
+    }
+    alert("Le groupe est modifié");
   }
 }
--- src/app/groupe-musique/groupe-musique.module.ts
+++ src/app/groupe-musique/groupe-musique.module.ts
@@ -4,16 +4,18 @@ import {ListeGroupeMusiqueComponent} from "./liste-groupe-musique/liste-groupe-m
 import {GroupeMusiqueService} from "./liste-groupe-musique/groupe-musique.service";
 import {GroupeMusiqueDetailComponent} from "./groupe-musique-detail/groupe-musique-detail.component";
 import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     ListeGroupeMusiqueComponent,
     GroupeMusiqueDetailComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        ReactiveFormsModule
+    ],
   providers: [GroupeMusiqueService],
 })
 export class GroupeMusiqueModule { }
